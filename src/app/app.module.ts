564c9e0d897047a1434fe612c69ebea8952e56e7
shreya1798 <shreyasgnr@gmail.com>
2019-02-21 10:53:19 +0530

    basic navbar and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponentComponent } from './nav-component/nav-component.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
